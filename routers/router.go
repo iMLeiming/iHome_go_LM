445f549aa93c4d44dd45b482f4f2159d70ef5bd1
iMLeiming <imeamillei@163.com>
2018-01-08 15:20:09 +0800

    静态页面部署

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"iHome_go_LM/iHome_go_LM/controllers"
 	"github.com/astaxie/beego"
+	"iHome_go_LM/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
